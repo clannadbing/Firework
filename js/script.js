49246c4fba6ff71cb85701451e6b5589cec105e4
clannadbing <2830547211@qq.com>
2026-02-16 22:42:08 +0800

    init

--- js/script.js
+++ js/script.js
@@ -78,7 +78,7 @@ const mainStage = new Stage("main-canvas");
 const stages = [trailsStage, mainStage];
 
 //随机文字烟花内容
-const randomWords = [""];
+const randomWords = ["小谯同学，新年快乐!"];
 const wordDotsMap = {};
 randomWords.forEach((word) => {
 	wordDotsMap[word] = MyMath.literalLattice(word, 3, "Gabriola,华文琥珀", "90px");
